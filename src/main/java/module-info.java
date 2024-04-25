8dfed5e1e8f370b152b3831e5f14f120350f05ba
JavviFdeez <javifdez195@gmail.com>
2024-04-25 12:32:50 +0200

    Establecida la conexión con la base de datos

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module org.JavviFdeez {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires java.xml.bind;
 
     opens org.JavviFdeez to javafx.fxml;
     exports org.JavviFdeez;
