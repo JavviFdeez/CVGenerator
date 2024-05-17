12bc20e0802cba67933f566794051a20fd5ee4c8
JavviFdeez <javifdez195@gmail.com>
2024-05-17 23:23:06 +0200

    Actualización de la vista FormDataAcademies

--- src/main/java/org/JavviFdeez/controller/view/FormDataContactController.java
+++ src/main/java/org/JavviFdeez/controller/view/FormDataContactController.java
@@ -2,14 +2,18 @@ package org.JavviFdeez.controller.view;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import org.JavviFdeez.controller.ContactController;
 import org.JavviFdeez.model.connection.ConnectionMariaDB;
 import org.JavviFdeez.model.dao.ContactDAO;
@@ -113,13 +117,33 @@ public class FormDataContactController implements Initializable {
                 if (saveDataToDatabase) {
                     showAlert("Exito", "Los datos se han guardado exitosamente", Alert.AlertType.INFORMATION);
                     // Cambiar a la escena de inicio de sesión después de guardar el usuario
-                    // changeSceneToFormData();
+                    changeSceneToFormData();
                 } else {
                     showAlert("Error", "No se han podido guardar los datos", Alert.AlertType.ERROR);
                 }
         } catch (SQLException e) {
             e.printStackTrace();
             showAlert("Error", e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+
+    private void changeSceneToFormData() {
+        try {
+            // Cargar la nueva escena desde el archivo FXML
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/JavviFdeez/fxml/FormDataAcademies.fxml"));
+            Parent root = loader.load();
+
+            // Obtener el escenario actual desde el emailTextField (o cualquier otro nodo)
+            Stage stage = (Stage) emailTextField.getScene().getWindow();
+
+            // Establecer la nueva escena en el escenario
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Manejar cualquier error de carga del archivo FXML
+            showAlert("Error", "No se pudo cargar la pantalla de inicio de sesión.", Alert.AlertType.ERROR);
         }
     }
 
@@ -151,7 +175,6 @@ public class FormDataContactController implements Initializable {
 
                 // Guardar la ruta relativa
                 imageRelativePath = outputPath.toString();
-                showAlert("Exito", "Imagen guardada",  Alert.AlertType.INFORMATION);
             } catch (IOException e) {
                 e.printStackTrace();
             }
